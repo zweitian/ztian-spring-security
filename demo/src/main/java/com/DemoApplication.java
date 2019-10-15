4a2ac9328197f3b40da842a306b3ed2ccc0daaf0
zhuweitian <zhuweitian@qipeipu.com>
2019-10-16 00:54:56 +0800

    新增springApplication启动类

---
+++ demo/src/main/java/com/DemoApplication.java
@@ -0,0 +1,23 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ztian
+ * @date 2019/10/16
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("hello")
+	public String hello() {
+		return "hello";
+	}
+}
