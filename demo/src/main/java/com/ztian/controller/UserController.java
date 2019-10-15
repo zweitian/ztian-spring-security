401fdf1cb5d06b06a6748de02ed27a9c1119f96a
zhuweitian <zhuweitian@qipeipu.com>
2019-10-16 01:24:54 +0800

    spring mvc 测试类引入

---
+++ demo/src/main/java/com/ztian/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.ztian.controller;
+
+/**
+ * @author ztian
+ * @date 2019/10/16
+ */
+public class UserController {
+}
---
+++ demo/src/test/java/com/ztian/controller/UserControllerTest.java
@@ -0,0 +1,33 @@
+package com.ztian.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+/**
+ * @author ztian
+ * @date 2019/10/16
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@ActiveProfiles(value = {})
+public class UserControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void getHelloTest() throws Exception {
+		String contentAsString = mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andReturn().getResponse().getContentAsString();
+		System.out.println(contentAsString);
+	}
+}
