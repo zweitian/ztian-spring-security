76262bf2a65241efa329b34f652030eaa6be821f
zhuweitian <zhu.weitian@foxmail.com>
2019-10-19 01:48:53 +0800

    jsonview控制返回值字段输出

--- demo/src/main/java/com/ztian/controller/UserController.java
+++ demo/src/main/java/com/ztian/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ztian.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.google.common.collect.Lists;
 import com.ztian.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,14 +19,16 @@ import java.util.List;
 public class UserController {
 
 	@GetMapping
+	@JsonView(User.UserSimpleView.class)
 	public List<User> listUser() {
-		User user1 = new User(1, "张三", "");
-		User user2 = new User(2, "李四", "");
+		User user1 = new User(1, "张三", "123");
+		User user2 = new User(2, "李四", "123");
 		return Lists.newArrayList(user1, user2);
 	}
 
 	@GetMapping("{id:\\d+}")
+	@JsonView(User.UserDetailView.class)
 	public User getUser(@PathVariable("id") Integer id) {
-		return new User(id, "张三", "");
+		return new User(id, "张三", "123");
 	}
 }
--- demo/src/main/java/com/ztian/entity/User.java
+++ demo/src/main/java/com/ztian/entity/User.java
@@ -1,5 +1,6 @@
 package com.ztian.entity;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,7 +11,15 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class User {
+	public interface UserSimpleView {};
+	public interface UserDetailView extends UserSimpleView {};
+
+	@JsonView(UserSimpleView.class)
 	private Integer id;
+
+	@JsonView(UserSimpleView.class)
 	private String userName;
+
+	@JsonView(UserDetailView.class)
 	private String password;
 }
